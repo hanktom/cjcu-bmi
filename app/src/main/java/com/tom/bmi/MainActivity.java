2c4801729ecd0f76e4a86ff2d81a5e4200245f1d
Hank Tom <hanktom@gmail.com>
2017-05-20 11:55:07 +0800

    Bmi calculate

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -2,6 +2,9 @@ package com.tom.bmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void bmi(View view){
+        Log.d("HAHA", "bmi");
+        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
+        EditText edHeight = (EditText) findViewById(R.id.ed_height);
+        String sWeight = edWeight.getText().toString();
+        String sHeight = edHeight.getText().toString();
+        float height = Float.parseFloat(sHeight);
+        float weight = Float.parseFloat(sWeight);
+        float bmi = weight/(height*height);
+        Log.d("HAHA", "bmi:"+bmi);
     }
 }
