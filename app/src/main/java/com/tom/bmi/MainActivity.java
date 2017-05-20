4bf9946b85f07bb272c70c50c07d343dd74cbbe6
Hank Tom <hanktom@gmail.com>
2017-05-20 13:30:14 +0800

    Refactor, SharedPreferences

--- app/src/main/java/com/tom/bmi/MainActivity.java
+++ app/src/main/java/com/tom/bmi/MainActivity.java
@@ -1,5 +1,6 @@
 package com.tom.bmi;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,24 +8,37 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText edWeight;
+    private EditText edHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        float w = getSharedPreferences("aaa",MODE_PRIVATE)
+              .getFloat("weight", 0);
+        edWeight.setText(String.valueOf(w));
+    }
 
-
+    private void findViews() {
+        edWeight = (EditText) findViewById(R.id.ed_weight);
+        edHeight = (EditText) findViewById(R.id.ed_height);
     }
 
     public void bmi(View view){
         Log.d("HAHA", "bmi");
-        EditText edWeight = (EditText) findViewById(R.id.ed_weight);
-        EditText edHeight = (EditText) findViewById(R.id.ed_height);
         String sWeight = edWeight.getText().toString();
         String sHeight = edHeight.getText().toString();
         float height = Float.parseFloat(sHeight);
         float weight = Float.parseFloat(sWeight);
         float bmi = weight/(height*height);
         Log.d("HAHA", "bmi:"+bmi);
+//        SharedPreferences sp = getSharedPreferences("aaa", MODE_PRIVATE);
+        getSharedPreferences("aaa", MODE_PRIVATE).edit()
+                .putFloat("weight", weight)
+                .putFloat("height", height)
+                .apply();
+
     }
 }
